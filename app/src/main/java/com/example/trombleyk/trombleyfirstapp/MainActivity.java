3fab16bc2c5497855471990fd9143bded467adef
Kyle <trombleyk@bishopireton.org>
2017-05-21 13:50:05 -0400

    It works now...

--- app/src/main/java/com/example/trombleyk/trombleyfirstapp/MainActivity.java
+++ app/src/main/java/com/example/trombleyk/trombleyfirstapp/MainActivity.java
@@ -56,15 +56,35 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public String getHexVal(int i){
-        String answer = "";
-        int temp = 0;
-        while(i > 0) {
-            temp = i % 10;
-            i = i / 10;
-
+    public int getHexVal(String s){
+        int base = 16, count = 0, digit = 0, answer = 0;
+        for (int i  = s.length(); i >= 0; i--){
+            digit = getLetterVal(s.charAt(i));
+            answer += digit * Math.pow(base, count);
+            count++;
         }
-    return "";
+        return answer;
     }
+    public static int getLetterVal(char c){
+        switch(c){
+            case '1' : return 1;
+            case '2' : return 2;
+            case '3' : return 3;
+            case '4' : return 4;
+            case '5' : return 5;
+            case '6' : return 6;
+            case '7' : return 7;
+            case '8' : return 8;
+            case '9' : return 9;
+            case 'A' : return 10;
+            case 'B' : return 11;
+            case 'C' : return 12;
+            case 'D' : return 13;
+            case 'E' : return 14;
+            case 'F' : return 15;
+        }
+        return -1;
+    }
+}
 
 }
