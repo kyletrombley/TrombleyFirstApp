f256b3cd5fe1c09ec1e8aa074d0787c199e65eba
trombleykyle <trombleyk@bishopireton.org>
2017-05-23 14:35:21 -0400

    Made text boxes interact with the calculate button.

--- app/src/main/java/com/example/trombleyk/trombleyfirstapp/MainActivity.java
+++ app/src/main/java/com/example/trombleyk/trombleyfirstapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +29,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         final Button button = (Button) findViewById(R.id.cal);
+        final EditText dec = (EditText) findViewById(R.id.dec);
+        final EditText hex = (EditText) findViewById(R.id.hex);
+        final EditText bin = (EditText) findViewById(R.id.bin);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Snackbar.make(view, "I drive a chevrolet movie theater", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();            }
+            if(bin.getText().toString().equals("") && hex.getText().toString().equals("")) {
+                String input = dec.getText().toString();
+                int value = Integer.parseInt(input);
+              //  hex.setText();
+              //  bin.setText();
+            }
+            else if(bin.getText().toString().equals("")){
+                String input = hex.getText().toString();
+                dec.setText(hexToDec(input));
+                //bin.setText();
+            }
+            else{
+                String input = bin.getText().toString();
+               // hex.setText();
+                dec.setText(binToDec(input));
+                }
+            }
         });
     }
 
@@ -56,35 +76,58 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public int getHexVal(String s){
-        int base = 16, count = 0, digit = 0, answer = 0;
+    public static int hexToDec(String s){
+        int base = 16, exp = 0, digit = 0, answer = 0;
         for (int i  = s.length(); i >= 0; i--){
             digit = getLetterVal(s.charAt(i));
-            answer += digit * Math.pow(base, count);
-            count++;
+            answer += digit * Math.pow(base, exp);
+            exp++;
         }
         return answer;
     }
-    public static int getLetterVal(char c){
-        switch(c){
-            case '1' : return 1;
-            case '2' : return 2;
-            case '3' : return 3;
-            case '4' : return 4;
-            case '5' : return 5;
-            case '6' : return 6;
-            case '7' : return 7;
-            case '8' : return 8;
-            case '9' : return 9;
-            case 'A' : return 10;
-            case 'B' : return 11;
-            case 'C' : return 12;
-            case 'D' : return 13;
-            case 'E' : return 14;
-            case 'F' : return 15;
+    public static int binToDec(String s){
+        int base = 2, exp = 0, digit = 0, answer = 0;
+        for (int i  = s.length(); i >= 0; i--){
+            digit = getLetterVal(s.charAt(i));
+            answer += digit * Math.pow(base, exp);
+            exp++;
+        }
+        return answer;
+    }
+    public static int getLetterVal(char c) {
+        switch (c) {
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            case 'A':
+                return 10;
+            case 'B':
+                return 11;
+            case 'C':
+                return 12;
+            case 'D':
+                return 13;
+            case 'E':
+                return 14;
+            case 'F':
+                return 15;
         }
         return -1;
     }
-}
 
 }
